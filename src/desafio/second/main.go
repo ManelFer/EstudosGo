c55895b8ceb3bdcf1308e6eb576fdbf04cf4390b
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-12-21 15:13:06 -0300

    update

--- src/desafio/second/main.go
+++ src/desafio/second/main.go
@@ -7,10 +7,27 @@ import (
 	"fmt"
 )
 
-func main() {
+/*func main() {
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 {
 			fmt.Println(i)
 		}
 	}
+}*/
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("pinpan")
+		} else if i%3 == 0 {
+			fmt.Println("pin")
+		} else if i%5 == 0 {
+			fmt.Println("pan")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
+
+// segunda parte do desafio
+// precisa exibir de 1 a 100 e os multiplos de 3 aparecer pin e os multiplos de 5 pan e se for ambom 3 e 5 pinpan
