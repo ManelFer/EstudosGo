971813764a1e3172897696f47935d70f6aab2ac0
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2026-01-01 22:36:00 -0300

    update
    finalização da api de pokemon

--- src/api/pokeApi.go
+++ src/api/pokeApi.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
+
+type Response struct {
+	Nome     string    `json:"name"`
+	Pokemons []Pokemon `json:"pokemon_entries"`
+}
+
+type Pokemon struct {
+	numero  int            `json:"entry_number"`
+	especie PokemonSpecies `json:"pokemon_species"`
+}
+
+type PokemonSpecies struct {
+	Nome string `json:"name"`
+}
 
 func main() {
 	res, err := http.Get("https://pokeapi.co/api/v2/pokedex/kanto/") // response mapeado en JSON
@@ -20,4 +35,13 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(string(responseData))
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+	fmt.Println(responseObject.Nome)
+	fmt.Println(len(responseObject.Pokemons))
+
+	for _, Pokemon := range responseObject.Pokemons {
+		fmt.Println(Pokemon.especie.Nome)
+	}
 }
