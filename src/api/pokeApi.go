1dd43bd19d603d03be44624a075cfef4ce3508aa
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2026-01-01 22:19:54 -0300

    ypdate

---
+++ src/api/pokeApi.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	res, err := http.Get("https://pokeapi.co/api/v2/pokedex/kanto/") // response mapeado en JSON
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+}
